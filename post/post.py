6f4c5d8cb1aef7ba633d64a5c0d24fa6b0f911cf
rgrubba <rgrubba@gmail.com>
2021-03-22 13:45:36 +0000

    automated description in metatags

--- post/post.py
+++ post/post.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from models import Page
 from sqlalchemy import extract
+from bs4 import BeautifulSoup
 import datetime
 
 post_bp = Blueprint('core_bp',__name__,template_folder='templates')
@@ -13,7 +14,7 @@ def index():
 def post(year,month,slug):
     requested_post = Page.query.filter(extract('year',Page.created)==year,extract('month',Page.created)==month).filter_by(slug=slug).first_or_404()
     if post:
-        return render_template('post/post.html',post=requested_post,date=datetime.datetime.strftime(requested_post.created, "%d %B %Y"))
+        return render_template('post/post.html',post=requested_post,date=datetime.datetime.strftime(requested_post.created, "%d %B %Y"),short=BeautifulSoup(requested_post.content,"html.parser").text.lstrip().rstrip()[0:52])
     else:
         return render_template('index.html')
 
@@ -21,6 +22,6 @@ def post(year,month,slug):
 def post_by_id(postid):
     requested_post = Page.query.filter_by(id=postid).first_or_404()
     if post:
-        return render_template('post/post.html',post=requested_post,date=datetime.datetime.strftime(requested_post.created, "%d %B %Y"))
+        return render_template('post/post.html',post=requested_post,date=datetime.datetime.strftime(requested_post.created, "%d %B %Y"),short=BeautifulSoup(requested_post.content,"html.parser").text.lstrip().rstrip()[0:52])
     else:
         return render_template('index.html')
