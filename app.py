f24e80490bd7b9d37513852e63584d0afd69da54
rgrubba <rgrubba@gmail.com>
2021-03-30 08:31:47 +0000

    polish month names and improved linking to homepage

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import calendar
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -9,7 +8,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.template_filter('month_name')
 def month_name(month_number):
-   return calendar.month_name[month_number]
+    miesiace = {
+            1:"Styczeń",
+            2:"Luty",
+            3:"Marzec",
+            4:"Kwiecień",
+            5:"Maj",
+            6:"Czerwiec",
+            7:"Lipiec",
+            8:"Sierpień",
+            9:"Wrzesień",
+            10:"Październik",
+            11:"Listopad",
+            12:"Grudzień"
+    }
+    return miesiace[month_number]
 
 db = SQLAlchemy(app)
 from models import Page, Tag, Category
